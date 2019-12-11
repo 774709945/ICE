c406817425f0960f01a12c767afad9f120fa3cf2
774709945 <774709945@qq.com>
2019-12-11 17:37:40 +0800

    解决方案更新

---
+++ ICE.Framework/ICE.Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserPwd { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public int IsDel { get; set; }
+
+        public DateTime? DelTime { get; set; }
+    }
+}
