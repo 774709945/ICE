b97962f9d9e8794881200c41763db3b95a613347
DongYu <774709945@qq.com>
2019-12-12 15:31:30 +0800

    更新

--- ICE.Framework/ICE.Framework/SqlBuilder.cs
+++ ICE.Framework/ICE.Framework/SqlBuilder.cs
@@ -16,17 +16,13 @@ namespace ICE.Framework
         static SqlBuilder()
         {
             Type tp = typeof(T);
-
             {
                 //查询sql语句
                 string columnsStr = string.Join(",", tp.GetProperties().Select(u => $"[{u.Name}]"));
 
                 _FindSql = $"SELECT {columnsStr} FROM [{tp.Name}] WHERE Id=";
             }
-            
         }
-
-
 
         /// <summary>
         /// 获取通用查询sql
